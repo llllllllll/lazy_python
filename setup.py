7294ba9ed02ab9731c027188bb61aa3e2d8c2b4e
llllllllll <joejev@gmail.com>
2015-01-27 20:05:57 -0500

    MAINT: Updates the requirments to remove six (no more py2 compat).

--- setup.py
+++ setup.py
@@ -23,17 +23,14 @@ setup(
         'Development Status :: 3 - Alpha'
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
         'Operating System :: POSIX',
         'Topic :: Software Development :: Pre-processors',
     ],
-    install_requires=[
-        'six',
-    ],
     url='https://github.com/llllllllll/lazy_python',
     ext_modules=[
         Extension('lazy._undefined', ['lazy/_undefined.c']),
-        Extension('lazy._thunk', ['lazy/_thunk.c'],
-                  extra_compile_args=['-O0']),
+        Extension('lazy._thunk', ['lazy/_thunk.c']),
     ],
 )
