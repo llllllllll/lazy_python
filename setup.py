f357d446ec7114b041c88cbdb0cac0656ed2eb15
llllllllll <joe@quantopian.com>
2015-02-20 20:59:32 -0500

    DEV: Updates to 0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if 'upload' in sys.argv:
 
 setup(
     name='lazy_python',
-    version='0.1.1',
+    version='0.1.2',
     description='Lazy evaluation for python 3',
     author='Joe Jevnik',
     author_email='joejev@gmail.com',
