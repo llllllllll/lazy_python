0aed2fac0a7c77fe2cdd76429e7b8a81489955a0
llllllllll <joe@quantopian.com>
2015-02-24 01:00:35 -0500

    TST: Fixes the broken tests.

--- lazy/tests/data/test_list.py
+++ lazy/tests/data/test_list.py
@@ -1,12 +1,11 @@
 from unittest import TestCase
 
-from lazy import undefined
 from lazy.data import LazyList, nil, Cons
 
 
 class LazyListTestCase(TestCase):
     def test_lazy_index(self):
-        ls = Cons(undefined, Cons(2, Cons(undefined, nil)))
+        ls = Cons(None, Cons(2, Cons(None, nil)))
         self.assertEqual(ls[1], 2)
 
     def test_LazyList_abstract(self):
--- lazy/tests/test_lazy_function.py
+++ lazy/tests/test_lazy_function.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 
-from lazy import thunk, lazy_function
+from lazy import thunk, strict, lazy_function
 
 
+# We decorate with strict so that the module loads, nose is a bitch.
+@strict
 @lazy_function
 def f(a, b):
     return a + b
@@ -19,3 +21,17 @@ class LazyFunctionTestCase(TestCase):
         g = lazy_function(g)
 
         self.assertIsInstance(g(1, 2), thunk)
+
+    def test_lazy_call(self):
+        called = False
+
+        @lazy_function
+        def g():
+            nonlocal called
+            called = True
+
+        result = g()
+        self.assertFalse(called, 'The function call was strict')
+
+        strict(result)
+        self.assertTrue(called, 'The function call did not evaluate')
