c9dadbe3a2a3bbeb74f238a0c4220224f6f63b9c
llllllllll <joe@quantopian.com>
2015-02-24 23:04:45 -0500

    TST: Adds tests for 'is' and 'not'.

--- lazy/tests/test_lazy_function.py
+++ lazy/tests/test_lazy_function.py
@@ -35,3 +35,45 @@ class LazyFunctionTestCase(TestCase):
 
         strict(result)
         self.assertTrue(called, 'The function call did not evaluate')
+
+    def test_is(self):
+        @lazy_function
+        def g():
+            return 1 is 1
+
+        a = g()
+        self.assertIsInstance(a, thunk)
+        self.assertTrue(strict(a))
+
+        @lazy_function
+        def h(a):
+            return a is None
+
+        b = h(None)
+        self.assertIsInstance(b, thunk)
+        self.assertTrue(strict(b))
+
+        c = h('not none')
+        self.assertIsInstance(c, thunk)
+        self.assertFalse(strict(c))
+
+    def test_not(self):
+        @lazy_function
+        def g():
+            return not 1
+
+        a = g()
+        self.assertIsInstance(a, thunk)
+        self.assertFalse(strict(a))
+
+        @lazy_function
+        def h(a):
+            return not a
+
+        b = h(False)
+        self.assertIsInstance(b, thunk)
+        self.assertTrue(strict(b))
+
+        c = h(True)
+        self.assertIsInstance(c, thunk)
+        self.assertFalse(strict(c))
