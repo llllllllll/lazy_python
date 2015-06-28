05dd502ddf81ad663cd726255d75aadacbcc1245
Joe Jevnik <joe@quantopian.com>
2015-06-27 21:37:31 -0400

    DEV: 0.1.11

--- lazy/__init__.py
+++ lazy/__init__.py
@@ -5,7 +5,7 @@ from lazy.bytecode import lazy_function
 from lazy.runtime import run_lazy
 from lazy.transformer import LazyTransformer
 
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 
 __all__ = [
     'LazyTransformer',
