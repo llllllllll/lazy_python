e1cab48b773cfbe9e4b9489c4834b6e38fbdd466
llllllllll <joe@quantopian.com>
2015-11-05 03:07:54 -0500

    BLD: upgrade version (not released)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if 'upload' in sys.argv:
 
 setup(
     name='lazy_python',
-    version='0.1.11',
+    version='0.1.12',
     description='Lazy evaluation for python 3',
     author='Joe Jevnik',
     author_email='joejev@gmail.com',
